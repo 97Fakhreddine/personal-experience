351a76a127c52e05aed0843800d07a9cd4a5bf42
97Fakhreddine <fakhri19971998@gmail.com>
2023-01-10 12:10:50 +0100

    portfolio has been added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,96 @@
 import React from 'react';
 import './portfolio.css';
+import IMG1 from '../../assets/images/portfolio1.jpg';
+import IMG2 from '../../assets/images/portfolio2.jpg';
+import IMG3 from '../../assets/images/portfolio3.jpg';
+import IMG4 from '../../assets/images/portfolio4.jpg';
+import IMG5 from '../../assets/images/portfolio5.png';
+import IMG6 from '../../assets/images/portfolio6.jpg';
 
 function Portfolio() {
-  return <section id='portfolio'>Portfolio</section>;
+  const portfolioData = [
+    {
+      id: '1',
+      image: IMG1,
+      title: 'Some titles',
+      github: 'repo link ... ',
+      demo: '',
+    },
+    {
+      id: '2',
+      image: IMG2,
+      title: 'Some titles',
+      github: 'repo link ... ',
+      demo: '',
+    },
+    {
+      id: '3',
+      image: IMG3,
+      title: 'Some titles',
+      github: 'repo link ... ',
+      demo: '',
+    },
+    {
+      id: '4',
+      image: IMG4,
+      title: 'Some titles',
+      github: 'repo link ... ',
+      demo: '',
+    },
+    {
+      id: '5',
+      image: IMG5,
+      title: 'Some titles',
+      github: 'repo link ... ',
+      demo: '',
+    },
+    {
+      id: '6',
+      image: IMG6,
+      title: 'Some titles',
+      github: 'repo link ... ',
+      demo: '',
+    },
+  ];
+
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {portfolioData.map((e, index) => {
+          return (
+            e && (
+              <article className="portfolio__item" key={e.id}>
+                <div className="portfolio__item-image">
+                  <img src={e.image} alt="" />
+                </div>
+                <h3>{e.title}</h3>
+                <div className="portfolio__item-cta">
+                  <a
+                    href={e.github}
+                    aria-disabled={!e.demo}
+                    className="btn"
+                    target="__blank"
+                  >
+                    Github
+                  </a>
+                  <a
+                    aria-disabled={!e.demo}
+                    href={e.demo}
+                    className="btn btn-primary"
+                    target="__blank"
+                  >
+                    Live Demo
+                  </a>
+                </div>
+              </article>
+            )
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio;
