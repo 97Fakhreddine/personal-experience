e3cb5b3beae4f5766126898a4602710a85c317a2
97Fakhreddine <fakhri19971998@gmail.com>
2023-01-10 11:38:03 +0100

    services has been added

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,122 @@
-import React from 'react'
-import './experience.css'
+import React from 'react';
+import { BsPatchCheckFill } from 'react-icons/bs';
+import './experience.css';
 
 function Experience() {
   return (
-    <div id='experience'>Experience</div>
-  )
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="container experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Css/Scss</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Javascript</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Angular/NGRx</h4>
+                <small>Advanced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>React/Redux</h4>
+                <small>Advanced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Vue</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/** Backend cards */}
+        <div className="container experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Node JS</h4>
+                <small>Advanced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Nest JS</h4>
+                <small>Advanced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Typescript</h4>
+                <small>Mid Level</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small>Advanced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small>Basic</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Python</h4>
+                <small>Basic</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Experience
+export default Experience;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,155 @@
 import React from 'react';
 import './services.css';
+import { BiCheck } from 'react-icons/bi';
 
 function Services() {
-  return <section id='services'>Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container container__services">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        {/** End of Web Dev */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Nulla Lorem commodo officia excepteur id minim exercitation
+                tempor in quis non.
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        {/** End of Web Dev */}
+      </div>
+    </section>
+  );
 }
 
 export default Services;
