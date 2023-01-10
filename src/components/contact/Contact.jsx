895591ec7e6e020ab0c04116af559a806b59641a
97Fakhreddine <fakhri19971998@gmail.com>
2023-01-10 17:12:39 +0100

    contact us has been added successfully

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,75 @@
-import React from 'react'
-import './contact.css'
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+import './contact.css';
+import { MdOutlineEmail } from 'react-icons/md';
+import { RiMessengerLine } from 'react-icons/ri';
+import { BsWhatsapp } from 'react-icons/bs';
+import { SERVICE_ID, TEMPLATE_ID, USER_ID } from '../../constants/constant';
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+    await emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID);
+    e.target.reset();
+  };
+
   return (
-    <section id='contact'>Contact</section>
-  )
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>fakhry.messaoudi@gmail.com</h5>
+            <a href="mailto:fakhry.messaoudi@gmail.com" target="__blank">
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>Faa khri</h5>
+            <a href="https://m.me/Fakhri.Shaarawy" target="__blank">
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp />
+            <h4>WhatsApp</h4>
+            <h5>+216 50 769 755</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone+21650769755"
+              target="__blank"
+            >
+              Send a message
+            </a>
+          </article>
+
+          {/** End of Contact */}
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="subject" required placeholder="Subject" />
+          <input type="text" name="name" required placeholder="Your Name" />
+          <input type="email" name="email" required placeholder="Your Email" />
+          <textarea
+            rows="7"
+            name="message"
+            required
+            placeholder="Your Message"
+          />
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
---
+++ src/constants/constant.js
@@ -0,0 +1,8 @@
+const SERVICE_ID = 'service_pomvvx7';
+const TEMPLATE_ID = 'template_fydzjh4';
+const USER_ID = '9JFl8KJThjUmZ-K_7';
+module.exports = {
+  SERVICE_ID,
+  TEMPLATE_ID,
+  USER_ID,
+};
