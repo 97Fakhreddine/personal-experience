605fe537c78310771317b9232583059efebc1e07
97Fakhreddine <fakhri19971998@gmail.com>
2023-01-10 17:42:46 +0100

    footer has been added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,61 @@
 import React from 'react';
 import './footer.css';
 
+import { FaFacebookF } from 'react-icons/fa';
+import { FiInstagram, FiLinkedin, FiGithub } from 'react-icons/fi';
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="" className="footer__logo">
+        Fakhry
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonial</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="">
+          <FaFacebookF />
+        </a>
+        <a href="">
+          <FiInstagram />
+        </a>
+        <a href="">
+          <FiLinkedin />
+        </a>
+
+        <a href="">
+          <FiGithub />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Made by Fakhreddine Messaoudi. All right reserved</small>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
