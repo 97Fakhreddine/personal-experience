5864eb37692c77b23d86a6a61249332f4060e5a1
Fakhreddine Messaoudi <fakhry.messaoudi@gmail.com>
2026-08-22 10:30:16 +0200

    build: migrate React entrypoint for Vite

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { I18nextProvider } from 'react-i18next';
+
+import App from './App';
+import './index.css';
+import { i18n } from './core/i18n/i18n';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>
+);
